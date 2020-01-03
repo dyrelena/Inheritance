5f57e8e428ef3ce61d2adae23782a24cfe6badc9
DyrElena <lena.d.job@gmail.com>
2020-01-03 23:29:49 +0200

    finished

--- InheritanceClassRoom/InheritanceClassRoom/Program.cs
+++ InheritanceClassRoom/InheritanceClassRoom/Program.cs
@@ -1,145 +1,167 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace InheritanceClassRoom
 {
     class Program
-    {
-        Pupil st1 = new Pupil();
-        
-
+    {     
         static void Main(string[] args)
         {
-             
-            Console.WriteLine("Hello World!");
+            ClassRoom DP146 = new ClassRoom(
+                new ExcelentPupil { FirstName = "Jon", LastName = "Smith", ReadPoints = 10, WritePoints = 9, RelaxPoints = 8},
+                new GoodPupil { FirstName = "Potter", LastName = "Garry", ReadPoints = 8, WritePoints = 7, RelaxPoints = 7 },
+                new BadPupil {FirstName = "Bob", LastName = "Window", ReadPoints = 5, WritePoints = 4, RelaxPoints = 9});
+            
+            foreach (Pupil element in DP146)
+            {
+                element.Study();
+                element.Read();
+                element.Write();
+                element.Relax();
+                Console.WriteLine("\n");
+            }
+
+            Console.ReadLine();
         }
     }
-
     class ClassRoom
-    { 
-        Pupil[] arrStudents = new Pupil[4];
-        int index = 0;
-        public ClassRoom(Pupil student1, Pupil student2) 
+    {
+        List<Pupil> pupilGroup = new List<Pupil>();
+
+        public IEnumerator GetEnumerator()
         {
-            //arrStudents = new Pupil[] {student1, student2};
-            arrStudents[0]
+            return pupilGroup.GetEnumerator();
         }
 
-        public ClassRoom(Pupil student1, Pupil student2, Pupil student3)
+        public ClassRoom(Pupil pupil1, Pupil pupil2)
         {
-            arrStudents = new Pupil[] { student1, student2, student3};
+            pupilGroup.Add(pupil1);
+            pupilGroup.Add(pupil2);
         }
+
+        public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3) : this(pupil1, pupil2)
+        {
+            pupilGroup.Add(pupil3);
+        }
+
+        public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3, Pupil pupil4) : this(pupil1, pupil2, pupil3)
+        {
+            pupilGroup.Add(pupil4);
+        }
+
     }
 
     class Pupil
     {
         string firstName;
-        string lastName;        
+        string lastName;
         double readPoints;
         double writePoints;
         double relaxPoints;
 
         public string FirstName { get => firstName; set => firstName = value; }
-        public string LastName { get => lastName; set => lastName = value; }        
+        public string LastName { get => lastName; set => lastName = value; }
         public double ReadPoints { get => readPoints; set => readPoints = value; }
         public double WritePoints { get => writePoints; set => writePoints = value; }
         public double RelaxPoints { get => relaxPoints; set => relaxPoints = value; }
 
         public virtual void Study()
         {
-            Console.WriteLine("Student Performance: {0}", ReadPoints + WritePoints);
+            Console.Write("Student Performance: {0}", ReadPoints + WritePoints);
         }
 
         public virtual void Read()
         {
-            Console.WriteLine("Reading: {0}", ReadPoints);
+            Console.Write("Reading: {0}", ReadPoints);
         }
 
         public virtual void Write()
         {
-            Console.WriteLine("Writening: {0}", WritePoints);
+            Console.Write("Writening: {0}", WritePoints);
         }
 
         public virtual void Relax()
         {
-            Console.WriteLine("Ability to relax: {0}", RelaxPoints);
+            Console.Write("Ability to relax: {0}", RelaxPoints);
         }
     }
 
-    class ExcelentPupil:Pupil
+    class ExcelentPupil : Pupil
     {
         public override void Study()
         {
             base.Study();
-            Console.WriteLine("Good job!");
+            Console.WriteLine(" - Good job");
         }
         public override void Read()
         {
             base.Read();
-            Console.WriteLine("High result");
+            Console.WriteLine(" - High result");
         }
 
         public override void Write()
         {
             base.Write();
-            Console.WriteLine("High result");
+            Console.WriteLine(" - High result");
         }
 
         public override void Relax()
         {
             base.Relax();
-            Console.WriteLine("Excelent!");
+            Console.WriteLine(" - It's time to relax");
         }
     }
 
-    class GoodPupil:Pupil
+    class GoodPupil : Pupil
     {
         public override void Study()
         {
             base.Study();
-            Console.WriteLine("Not bad!");
+            Console.WriteLine(" - Not bad");
         }
         public override void Read()
         {
             base.Read();
-            Console.WriteLine("Good result");
+            Console.WriteLine(" - Good result");
         }
 
         public override void Write()
         {
             base.Write();
-            Console.WriteLine("Good result");
+            Console.WriteLine(" - Good result");
         }
 
         public override void Relax()
         {
             base.Relax();
-            Console.WriteLine("Well done!");
+            Console.WriteLine(" - Well done");
         }
     }
 
-    class BadPupil:Pupil
-    {
+    class BadPupil : Pupil
+    {        
         public override void Study()
         {
             base.Study();
-            Console.WriteLine("Very weak!");
+            Console.WriteLine(" - Very weak!");
         }
         public override void Read()
         {
             base.Read();
-            Console.WriteLine("Try your best");
+            Console.WriteLine(" - Try your best");
         }
 
         public override void Write()
         {
             base.Write();
-            Console.WriteLine("Try your best");
+            Console.WriteLine(" - Try your best");
         }
 
         public override void Relax()
         {
             base.Relax();
-            Console.WriteLine("No time to relax");
+            Console.WriteLine(" - Too relaxed");
         }
     }
 }
